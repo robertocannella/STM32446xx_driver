41193e7d281bb255e11f3398ccec5fef66ff8544
Roberto Cannella <73362871+robertocannella@users.noreply.github.com>
2022-06-14 23:34:05 -0400

    Update STM32F446xx.h

--- drivers/Inc/STM32F446xx.h
+++ drivers/Inc/STM32F446xx.h
@@ -16,7 +16,7 @@
 #define SRAM1_BASEADDR				0x20000000U				// 112kb
 #define SRAM2_BASEADDR				0x2001C000U				// 16kb
 #define ROM_BASEADDR				0x1FFF0000U				// system memory
-#define SRAM						SRAM1_BASE_ADDR			// main ram
+#define SRAM          SRAM1_BASE_ADDR			// main ram
 
 /*
  * Peripheral Base Address  (pages 57-58)
