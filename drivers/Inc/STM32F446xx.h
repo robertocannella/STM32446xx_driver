9e5b008dcd6f514857037442abefc7b65c35b5fd
robertocannella <robertocannella@gmail.com>
2022-06-14 22:38:40 -0400

    Added main memory macros

--- drivers/Inc/STM32F446xx.h
+++ drivers/Inc/STM32F446xx.h
@@ -7,7 +7,15 @@
 
 #ifndef INC_STM32F446XX_H_
 #define INC_STM32F446XX_H_
+#include <stdint.h>
+/*
+ * Base Address of Flash and SRAM Memory (page 62)
+ */
 
-
+#define FLASH_BASEADDR						0x08000000U
+#define SRAM1_BASEADDR						0x20000000U				// 112kb
+#define SRAM2_BASEADDR						0x2001C000U				// 16kb
+#define ROM_BASEADDR						0x1FFF0000U				// system memory
+#define SRAM								SRAM1_BASE_ADDR			// main ram
 
 #endif /* INC_STM32F446XX_H_ */
