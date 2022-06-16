c63d342c27a2bcae78a8b2daf65ae26b3e121506
robertocannella <robertocannella@gmail.com>
2022-06-16 10:54:13 -0400

    Added GPIO driver source/header files

---
+++ drivers/Inc/STM32F446xx_gpio_driver.h
@@ -0,0 +1,13 @@
+/*
+ * STM32F446xx_gpio_driver.h
+ *
+ *  Created on: Jun 16, 2022
+ *      Author: robertocannella
+ */
+
+#ifndef INC_STM32F446XX_GPIO_DRIVER_H_
+#define INC_STM32F446XX_GPIO_DRIVER_H_
+
+#include "STM32F446xx.h"
+
+#endif /* INC_STM32F446XX_GPIO_DRIVER_H_ */
---
+++ drivers/Src/STM32F446xx_gpio_driver.c
@@ -0,0 +1,11 @@
+/*
+ * STM32F446xx_gpio_driver.c
+ *
+ *  Created on: Jun 16, 2022
+ *      Author: robertocannella
+ */
+
+#include "STM32F446xx_gpio_driver.h"
+
+
+
