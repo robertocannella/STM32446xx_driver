0bc7a0e75ef925130087135b5f6ef20e8377c3f6
robertocannella <robertocannella@gmail.com>
2022-06-16 21:58:41 -0400

    Added GPIO function prototypes

--- drivers/Inc/STM32F446xx_gpio_driver.h
+++ drivers/Inc/STM32F446xx_gpio_driver.h
@@ -10,4 +10,53 @@
 
 #include "STM32F446xx.h"
 
+typedef struct {
+	uint8_t GPIO_PinNumber;
+	uint8_t GPIO_PinMode;
+	uint8_t GPIO_PinSpeed;
+	uint8_t GPIO_PinPuPdCtrl;
+	uint8_t GPIO_PinOutPutTypeCtrl;
+	uint8_t GPIO_PinAltFuncMode;
+
+}GPIO_PinConfig_t;
+
+/*
+ * The handle structure for a GPIO pin
+ */
+
+typedef struct{
+
+	GPIO_RegDef_t *pGPIOx;                 // The base address of the GPIO to which the pin belongs
+	GPIO_PinConfig_t GPIOPinConfig;        // The GPIO pin configuration settings
+
+}GPIO_handle_t;
+
+
+/*****************************************************************************************
+ *
+ * 				API Supported by this driver
+ *
+ */
+
+// Peripheral clock
+void GPIO_PeriClkCtrl(void);              // Enable the GPIO Peripheral Clock
+
+// Initialize/DeInit
+void GPIO_Init(void);
+void GPIO_DeInit(void);
+
+// Read/Write
+void GPIO_ReadFromInputPin(void);
+void GPIO_ReadFromInputPort(void);
+void GPIO_WriteToOutputPin(void);
+void GPIO_WriteToOutputPort(void);
+void GPIO_ToggleOutputPin(void);
+
+// IRQ/ISR Handling
+void GPIO_IRQConfig(void);                 // Interrupt configuration
+void GPIO_InterruptHandling(void);         // Handler for interrupt trigger condition
+
+
+
+
 #endif /* INC_STM32F446XX_GPIO_DRIVER_H_ */
